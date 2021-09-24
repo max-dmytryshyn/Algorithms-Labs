949af5a523e082c99ab755fea2d449af8728707b
max-dmytryshyn <maksumus22@gmail.com>
2021-09-24 10:00:23 +0300

    Create binary search tree with search, input, find max/min value methods

---
+++ Practice/binary_search_tree.py
@@ -0,0 +1,81 @@
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def search(self, value):
+        if self._search(self.root, value) is None:
+            return False
+        return True
+
+    def _search(self, node_to_search, value):
+        if node_to_search is None or value == node_to_search.value:
+            return node_to_search
+
+        if value > node_to_search.value:
+            return self._search(node_to_search.right_child, value)
+
+        if value < node_to_search.value:
+            return self._search(node_to_search.left_child, value)
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+            return
+
+        if self.search(value):
+            return
+
+        self._insert(self.root, value)
+
+    def _insert(self, current_node, value):
+        if value > current_node.value:
+            if current_node.right_child is None:
+                current_node.right_child = Node(value, current_node)
+            return self._insert(current_node.right_child, value)
+
+        if value < current_node.value:
+            if current_node.left_child is None:
+                current_node.left_child = Node(value, current_node)
+            return self._insert(current_node.left_child, value)
+
+    def delete(self, value):
+        pass
+
+    def max_value(self):
+        if self.root is None:
+            return None
+
+        current_node = self.root
+        while current_node.right_child is not None:
+            current_node = current_node.right_child
+
+        return current_node.value
+
+    def min_value(self):
+        if self.root is None:
+            return None
+
+        current_node = self.root
+        while current_node.left_child is not None:
+            current_node = current_node.left_child
+
+        return current_node.value
+
+
+class Node:
+
+    def __init__(self, value, parent=None):
+        self.left_child = None
+        self.right_child = None
+        self.parent = parent
+        self.value = value
+
+
+bst = BinarySearchTree()
+bst.insert(8)
+bst.insert(9)
+bst.insert(6)
+bst.insert(10)
+bst.insert(-2)
+bst.insert(3)
