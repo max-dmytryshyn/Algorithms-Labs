367889760e0e32742a3817b0998430ee1186915a
max-dmytryshyn <maksumus22@gmail.com>
2021-10-19 21:20:43 +0300

    Add processing of input file

--- Lab_4/dijkstra_algorithm.py
+++ Lab_4/dijkstra_algorithm.py
@@ -49,60 +49,17 @@ class Graph:
 
 
 if __name__ == "__main__":
-    a = Vertex("A")
-    b = Vertex("B")
-    c = Vertex("C")
-    d = Vertex("D")
-    e = Vertex("E")
-    f = Vertex("F")
-    g = Vertex("G")
-    h = Vertex("H")
-    i = Vertex("I")
-    vertexes = [a, b, c, d, e, f, g, h, i]
-
-    a.add_edge(b, 4)
-    a.add_edge(h, 8)
-
-    b.add_edge(a, 4)
-    b.add_edge(c, 8)
-    b.add_edge(h, 11)
-
-    c.add_edge(b, 8)
-    c.add_edge(d, 7)
-    c.add_edge(f, 4)
-    c.add_edge(i, 2)
-
-    d.add_edge(c, 7)
-    d.add_edge(e, 9)
-    d.add_edge(f, 14)
-
-    e.add_edge(d, 9)
-    e.add_edge(f, 10)
-
-    f.add_edge(c, 4)
-    f.add_edge(d, 14)
-    f.add_edge(e, 10)
-    f.add_edge(g, 2)
-
-    g.add_edge(f, 2)
-    g.add_edge(h, 1)
-    g.add_edge(i, 6)
-
-    h.add_edge(a, 8)
-    h.add_edge(b, 11)
-    h.add_edge(g, 1)
-    h.add_edge(i, 7)
-
-    i.add_edge(c, 2)
-    i.add_edge(g, 6)
-    i.add_edge(h, 7)
-
     graph = Graph()
-    for vertex in vertexes:
-        graph.add_vertex(vertex)
+    input_file = open("./test_input.txt", "r", encoding='utf-8-sig')
+    edges_amount, start_vertex = input_file.readline().split()
+    edges_amount = int(edges_amount)
+    for _ in range(edges_amount):
+        edge_start, edge_end, edge_weight = input_file.readline().split()
+        if edge_start not in graph.vertexes.keys():
+            graph.vertexes[edge_start] = Vertex(edge_start)
+        if edge_end not in graph.vertexes.keys():
+            graph.vertexes[edge_end] = Vertex(edge_end)
+        graph.vertexes[edge_start].add_edge(graph.vertexes[edge_end], float(edge_weight))
 
-    for vertex in vertexes:
-        print("Vertex:", vertex.name)
-        for item in graph.dijkstra_algorithm(vertex.name).items():
-            print(item[0] + ":", item[1])
-        print("----------------------------------")
+    distances = [distance for distance in graph.dijkstra_algorithm(start_vertex).values() if distance != inf]
+    print(sum(distances)/(len(distances)-1))
